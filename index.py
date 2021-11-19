61d5954c0dc0848aeabe00d29ffcc6f0cea0f830
paul88vera <paul88vera@gmail.com>
2021-11-19 09:58:19 -0600

    initial commit

---
+++ index.py
@@ -0,0 +1,6 @@
+# create a new input that takes an float (decimal number)
+grossIncome = float(input("Enter your gross income"))
+
+# change the gross income to the net income using .0625
+netIncome = grossIncome - (grossIncome * .0625)
+
